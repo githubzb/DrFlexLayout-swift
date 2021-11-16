2b1d13e404961e08e76b76667623009c25e9e474
zhangbao <1126976340@qq.com>
2021-11-16 22:34:15 +0800

    增加部分代码

--- DrFlexLayout/src/UI/DrFlexTableView.swift
+++ DrFlexLayout/src/UI/DrFlexTableView.swift
@@ -10,11 +10,21 @@ import UIKit
 fileprivate let kDrFlexCellIdentifier = "DrFlexTableView.flexCell"
 fileprivate let kDrFlexHeaderFooterIdentifier = "DrFlexTableView.flexHeaderFooter"
 
+fileprivate typealias DrFlexCellList = NSMutableArray
+
 public class DrFlexTableView: UIView {
     
     private let table: UITableView
     /// 内部的UITableView（注意：请不要直接改变其相关的代理方法，否则内部将无法正确工作）
     public var innerTable: UITableView { table }
+    
+    /// 存储每组cell视图
+    private lazy var cellViewMap: [Int: DrFlexCellList] = [:]
+    private lazy var headerViewList: [UIView] = []
+    private lazy var footerViewList: [UIView] = []
+    
+    private var dataSource: DrFlexTableDataSource?
+    private var delegate: DrFlexTableDelegate?
     
     public init(style: UITableView.Style) {
         table = UITableView(frame: .zero, style: style)
@@ -31,8 +41,8 @@ public class DrFlexTableView: UIView {
         table.tableFooterView = UIView()
         super.init(frame: .zero)
         addSubview(table)
-        table.delegate = self
-        table.dataSource = self
+        self.dataSource = DrFlexTableDataSource(flexTable: self)
+        self.delegate = DrFlexTableDelegate(flexTable: self)
     }
     
     required init?(coder: NSCoder) {
@@ -46,7 +56,55 @@ public class DrFlexTableView: UIView {
         layoutTableFooterView()
     }
     
+    public override func didMoveToWindow() {
+        super.didMoveToWindow()
+        table.dataSource = dataSource
+        table.delegate = delegate
+    }
+    
+    // MARK: - 回调函数
+    
+    /// 返回分组个数
+    fileprivate var numberOfSections: (()->Int)?
+    /// 绑定分组个数回调
+    public func numberOfSections<T: AnyObject>(_ target: T, binding: @escaping (T)->Int) {
+        weak var weakTarget = target
+        self.numberOfSections = {
+            if let target = weakTarget {
+                return binding(target)
+            }
+            return 0
+        }
+    }
+    /// 返回每组下的cell个数
+    fileprivate var numberOfRowsInSection: ((Int)->Int)?
+    /// 绑定每组下的cell个数回调
+    public func numberOfRowsInSection<T: AnyObject>(_ target: T, binding: @escaping (_ target: T, _ section: Int)->Int) {
+        weak var weakTarget = target
+        self.numberOfRowsInSection = { (section) in
+            if let target = weakTarget {
+                return binding(target, section)
+            }
+            return 0
+        }
+    }
+    /// cell点击
+    fileprivate var cellClick: ((IndexPath)->Void)?
+    /// 绑定cell点击回调
+    public func cellClick<T: AnyObject>(_ target: T, binding: @escaping (_ target: T, _ indexPath: IndexPath)->Void) {
+        weak var weakTarget = target
+        self.cellClick = { (indexPath) in
+            if let target = weakTarget {
+                binding(target, indexPath)
+            }
+        }
+    }
+    
     // MARK: - Tools
+    
+    public func reload() {
+        table.reloadData()
+    }
     
     /// 重新计算tableHeaderView布局
     public func layoutTableHeaderView() {
@@ -68,6 +126,79 @@ public class DrFlexTableView: UIView {
             table.tableFooterView = footer
             table.endUpdates()
         }
+    }
+    
+    /**
+     获取指定indexPath的cell视图
+     
+     - Parameter indexPath: IndexPath
+     
+     - Returns: cell视图
+     */
+    public func cell<T: UIView>(at indexPath: IndexPath) -> T? {
+        cell(atRow: indexPath.row, atSection: indexPath.section)
+    }
+    
+    /**
+     获取指定组下的指定行的cell视图
+     
+     - Parameter row: 行下标
+     - Parameter section: 组下标
+     
+     - Returns: cell视图
+     */
+    public func cell<T: UIView>(atRow row: Int, atSection section: Int) -> T? {
+        guard let list = cellViewMap[section] else { return nil }
+        guard list.count > row else { return nil }
+        guard let cell = list[row] as? UIView else { return nil }
+        return cell as? T
+    }
+    
+    /**
+     获取指定组的HeaderView
+     
+     - Parameter section: 组下标
+     
+     - Returns: HeaderView
+     */
+    public func headerView<T: UIView>(atSection section: Int) -> T? {
+        if section < headerViewList.count, section >= 0 {
+            return headerViewList[section] as? T
+        }
+        return nil
+    }
+    
+    /**
+     获取指定组的FooterView
+     
+     - Parameter section: 组下标
+     
+     - Returns: FooterView
+     */
+    public func footerView<T: UIView>(atSection section: Int) -> T? {
+        if section < footerViewList.count, section >= 0 {
+            return footerViewList[section] as? T
+        }
+        return nil
+    }
+    
+    /// 存储cell视图
+    fileprivate func appendCell(cell: UIView, atIndexPath indexPath: IndexPath) {
+        if let list = cellViewMap[indexPath.section] {
+            list.add(cell)
+        }else{
+            let list = NSMutableArray()
+            list.add(cell)
+            cellViewMap[indexPath.section] = list
+        }
+    }
+    /// 存储headerView
+    fileprivate func appendHeaderView(header: UIView) {
+        headerViewList.append(header)
+    }
+    /// 存储footerView
+    fileprivate func appendFooterView(footer: UIView) {
+        footerViewList.append(footer)
     }
     
     // MARK: - TableView Paramters
@@ -189,11 +320,9 @@ public class DrFlexTableView: UIView {
     
     public var tableHeaderView: UIView? {
         set {
-            if let view = newValue, view.isYogaEnabled {
-                if bounds.size != .zero {
-                    view.dr_resetWidth(bounds.width)
-                    view.dr_flex.layout(mode: .adjustHeight)
-                }
+            if let view = newValue, view.isYogaEnabled, bounds.size != .zero {
+                view.dr_resetWidth(bounds.width)
+                view.dr_flex.layout(mode: .adjustHeight)
                 table.tableHeaderView = view
             }else {
                 table.tableHeaderView = newValue
@@ -206,11 +335,9 @@ public class DrFlexTableView: UIView {
     
     public var tableFooterView: UIView? {
         set {
-            if let view = newValue, view.isYogaEnabled {
-                if bounds.size != .zero {
-                    view.dr_resetWidth(bounds.width)
-                    view.dr_flex.layout(mode: .adjustHeight)
-                }
+            if let view = newValue, view.isYogaEnabled, bounds.size != .zero {
+                view.dr_resetWidth(bounds.width)
+                view.dr_flex.layout(mode: .adjustHeight)
                 table.tableFooterView = view
             }else {
                 table.tableFooterView = newValue
@@ -221,6 +348,53 @@ public class DrFlexTableView: UIView {
         }
     }
 }
+
+
+// MARK: - TableView DataSource
+
+fileprivate class DrFlexTableDataSource: NSObject, UITableViewDataSource {
+    
+    weak var flexTable: DrFlexTableView?
+    
+    init(flexTable: DrFlexTableView) {
+        self.flexTable = flexTable
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        flexTable?.numberOfSections?() ?? 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        flexTable?.numberOfRowsInSection?(section) ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: kDrFlexCellIdentifier, for: indexPath)
+        cell.contentView.backgroundColor = indexPath.row % 2 == 0 ? .green : .blue
+        return cell
+    }
+    
+}
+
+// MARK: - TableView Delegate
+
+fileprivate class DrFlexTableDelegate: NSObject, UITableViewDelegate {
+    
+    weak var flexTable: DrFlexTableView?
+    
+    init(flexTable: DrFlexTableView) {
+        self.flexTable = flexTable
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        flexTable?.cellClick?(indexPath)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+}
+
 
 
 fileprivate extension UIView {
@@ -234,19 +408,3 @@ fileprivate extension UIView {
     
 }
 
-
-extension DrFlexTableView: UITableViewDelegate, UITableViewDataSource {
-    
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        30
-    }
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: kDrFlexCellIdentifier, for: indexPath)
-        cell.contentView.backgroundColor = indexPath.row % 2 == 0 ? .orange : .green
-        return cell
-    }
-    
-    public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        50
-    }
-}
--- DrFlexLayoutDemo/demo/FlexTableViewController.swift
+++ DrFlexLayoutDemo/demo/FlexTableViewController.swift
@@ -18,25 +18,53 @@ class FlexTableViewController: UIViewController {
         super.viewDidLoad()
         view.addSubview(table)
 
-        let header = UIView()
-        header.backgroundColor = .blue
-        header.dr_flex.addItem(self.v).height(100).marginHorizontal(20).marginVertical(20).backgroundColor(.yellow)
-        table.tableHeaderView = header
-
-        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
-            self.table.tableHeaderView?.backgroundColor = .red
-            self.v.backgroundColor = .blue
-            self.v.dr_flex.height(200)
-            let v = UIView()
-            v.backgroundColor = .green
-            self.table.tableHeaderView?.dr_flex.addItem(v).height(20).marginHorizontal(20).marginVertical(20)
-            self.table.layoutTableHeaderView()
+//        let header = UIView()
+//        header.backgroundColor = .blue
+//        header.dr_flex.addItem(self.v).height(100).marginHorizontal(20).marginVertical(20).backgroundColor(.yellow)
+//        table.tableHeaderView = header
+//
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+//            self.table.tableHeaderView?.backgroundColor = .red
+//            self.v.backgroundColor = .blue
+//            self.v.dr_flex.height(200)
+//            let v = UIView()
+//            v.backgroundColor = .green
+//            self.table.tableHeaderView?.dr_flex.addItem(v).height(20).marginHorizontal(20).marginVertical(20)
+//            self.table.layoutTableHeaderView()
+//        }
+        
+        table.numberOfRowsInSection(self) { target, section in
+            return target.number
         }
         
+        table.cellClick(self) { target, indexPath in
+            target.clickCell(indexPath: indexPath)
+        }
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "刷新",
+                                                                 style: .plain,
+                                                                 target: self,
+                                                                 action: #selector(refresh))
+    }
+    
+    @objc func refresh(){
+        table.reload()
+    }
+    
+    var number: Int {
+        50
+    }
+    
+    func clickCell(indexPath: IndexPath) {
+        print("------点击：\(indexPath.row)")
     }
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
         table.frame = view.bounds
+    }
+    
+    deinit {
+        print("----deinit")
     }
 }
